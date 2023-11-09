5e66ac0afc2fd690427e5814598741a3ad2104d4
Ben Cyrus <62337982+bencyrus@users.noreply.github.com>
2023-11-09 05:10:04 +0000

    added nginx

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: 'http://backend:8080/:path*',
+			},
+		]
+	},
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
