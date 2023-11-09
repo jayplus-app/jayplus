5bff080678bd17ee64568b3c293495671349df16
Ben Cyrus <62337982+bencyrus@users.noreply.github.com>
2023-11-09 14:52:27 +0000

    setup prettier

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	async rewrites() {
-		return [
-			{
-				source: '/api/:path*',
-				destination: 'http://backend:8080/:path*',
-			},
-		]
-	},
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://backend:8080/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
--- website/middleware.ts
+++ website/middleware.ts
@@ -1,10 +1,10 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/admin/booking", request.url));
+	return NextResponse.redirect(new URL('/admin/booking', request.url))
 }
 
 export const config = {
-  matcher: "/admin/dashboard",
-};
+	matcher: '/admin/dashboard',
+}
