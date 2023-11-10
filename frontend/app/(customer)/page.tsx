aec6309ec49ef5f5154e65926556938d4c328dfd
Ben Cyrus <62337982+bencyrus@users.noreply.github.com>
2023-11-10 17:18:28 -0500

    added fetch to frontend

--- frontend/app/(customer)/page.tsx
+++ frontend/app/(customer)/page.tsx
@@ -1,5 +1,15 @@
 'use client'
+
+import { useEffect } from "react"
+
 export default function Page() {
+  useEffect(() => {
+    fetch('/api/booking/vehicle-types')
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err))
+  }, [])
+  
   return (
     <div>
       <h1>Customer App frontend</h1>
