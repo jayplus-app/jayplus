3150af3a2d5ac151f318d0c689a19295dc55d02b
Ben Cyrus <bencyrus@Bens-MacBook-Air.local>
2023-11-10 17:06:57 -0500

    added backend

---
+++ backend/contracts/booking/interfaces.go
@@ -0,0 +1,19 @@
+package booking
+
+import (
+	"backend/contracts/db"
+	"net/http"
+)
+
+type BookingInterface interface {
+	// Handlers
+	VehicleTypes(w http.ResponseWriter, r *http.Request, db db.DBInterface)
+	ServiceTypes(w http.ResponseWriter, r *http.Request, db db.DBInterface)
+	TimeSlots(w http.ResponseWriter, r *http.Request, db db.DBInterface)
+	ServiceCost(w http.ResponseWriter, r *http.Request, db db.DBInterface)
+	CreateBooking(w http.ResponseWriter, r *http.Request, db db.DBInterface)
+	CreateBookingAdmin(w http.ResponseWriter, r *http.Request, db db.DBInterface)
+	Bookings(w http.ResponseWriter, r *http.Request, db db.DBInterface)
+	Booking(w http.ResponseWriter, r *http.Request, db db.DBInterface)
+	CancelBooking(w http.ResponseWriter, r *http.Request, db db.DBInterface)
+}
